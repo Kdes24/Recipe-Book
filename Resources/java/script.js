bef1e64901f702967438f640a72e5fe9ca3c6d13
Kevin <kmdesbiens@gmail.com>
2023-05-22 16:05:56 -0400

    commit

---
+++ Resources/java/script.js
@@ -0,0 +1,11 @@
+var apikey="3a53eee8a3msh8d1bef2c9da9ba3p14b960jsna30ee2e5e1be"
+
+$(function() {
+    console.log("-----------------------");
+    console.log("html is loading");
+})
+
+$("#searchButton").click(function(){
+    console.log("-----------------------");
+    console.log('search button has been clicked');
+})
